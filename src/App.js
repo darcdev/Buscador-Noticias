dbcd9ff7d7257e6067647fda2cd120b30f47a9b1
Diego Andres Cabrera Rojas <diegoandresrojas2000@gmail.com>
2020-11-20 13:21:25 -0500

    consultando api y prop types

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
+import ListadoNoticias from "./components/ListadoNoticias";
+
+const API_KEY = "e302bb6b91da47cdab9e70bdf504e38a";
 
 function App() {
+  // definit categoria y noticias
+
+  const [categoria, guardarCategoria] = useState("");
+  const [noticias, guardarNoticias] = useState([]);
+
+  useEffect(() => {
+    const consultarAPI = async () => {
+      const url = `http://newsapi.org/v2/top-headlines?country=co&category=${categoria}&apiKey=${API_KEY}`;
+      const noticias = await (await fetch(url)).json();
+      guardarNoticias(noticias.articles);
+    };
+    consultarAPI();
+  }, [categoria]);
   return (
     <>
       <Header titulo="Buscador de Noticias" />
 
       <div className="container white">
-        <Formulario />
+        <Formulario guardarCategoria={guardarCategoria} />
+
+        <ListadoNoticias noticias={noticias} />
       </div>
     </>
   );
